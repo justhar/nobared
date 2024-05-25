a6ab2ee3821b5d2e40e13b8bb9305a7d6f37ee0d
harosky <95089977+justhar@users.noreply.github.com>
2024-05-25 13:10:51 +0700

    fixes some bugs

--- src/lib/models/user.ts
+++ src/lib/models/user.ts
@@ -1,54 +1,57 @@
-import mongoose from 'mongoose';
-import { MongodbAdapter } from '@lucia-auth/adapter-mongodb';
- 
+import mongoose from "mongoose";
+import { MongodbAdapter } from "@lucia-auth/adapter-mongodb";
+
 interface User {
-  _id: string,
-  email: string,
-  picture: string,
-  username: string,
+  _id: string;
+  email: string;
+  picture: string;
+  username: string;
 }
- 
+
 const UserSchema = new mongoose.Schema<User>({
   _id: {
     type: String,
-        required: true,
+    required: true,
   },
   email: {
     type: String,
-        required: true,
+    required: true,
   },
   picture: {
     type: String,
-        required: true,
+    required: true,
   },
   username: {
     type: String,
-        required: true,
+    required: true,
   },
   //. . . other fields
 });
- 
-export default mongoose.models.User || mongoose.model<User>('User', UserSchema);
- 
+
+export default mongoose.models.User || mongoose.model<User>("User", UserSchema);
+
 //models/session.ts
- 
+
 interface Session {
   user_id: string;
   expires_at: Date;
 }
- 
-export const SessionSchema = new mongoose.Schema<Session>({
-  user_id: {
-    type: String,
-    required: true,
+
+export const SessionSchema = new mongoose.Schema<Session>(
+  {
+    user_id: {
+      type: String,
+      required: true,
+    },
+    expires_at: {
+      type: Date,
+      required: true,
+    },
   },
-  expires_at: {
-    type: Date,
-    required: true,
-  },
-}, {timestamps: true});
+  { timestamps: true }
+);
 // adapter for lucia
 export const adapter = new MongodbAdapter(
-  mongoose.connection.collection('sessions'),
-  mongoose.connection.collection('users')
+  mongoose.connection.collection("sessions"),
+  mongoose.connection.collection("users")
 );
